1ec3362a38a06fbace23fcdabaf918286fe0bc28
darkfurry <87731765+darkfurry@users.noreply.github.com>
2023-08-31 20:45:40 +0300

    Add files via upload

---
+++ ZeynMacro.py
@@ -0,0 +1,15 @@
+﻿
+import pyautogui as pag
+import time
+import keyboard
+import sys
+
+pag.alert('press Q once! /press B to stop /DELAY 600sec!!')
+while True:
+   keyboard.wait('q')
+   while not keyboard.is_pressed('b'):
+            keyboard.press('ctrl+v')
+            pag.press('enter')
+            time.sleep(600)
+   pag.alert('end program')
+   sys.exit()
